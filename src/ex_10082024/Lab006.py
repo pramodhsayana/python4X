6b2dd3e16cf8876cfb40601116c5b6a0aefa7ed2
pramodhsayana <pramodh.sayana@gmail.com>
2024-08-14 22:12:30 +0530

    adding

---
+++ src/ex_10082024/Lab006.py
@@ -0,0 +1,13 @@
+age= 65
+# 111 = 33 -> not possible - A-Z OR a-z
+_= 11
+a = _ + 1
+print(a)
+
+name ="pk"
+pi = 3.22
+iscorrect = True
+# print(type(name)(pi)(age)(iscorrect))
+print(type(pi))
+print(type(age))
+print(type(iscorrect))

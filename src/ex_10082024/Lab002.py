0e1c84d97d5034f527720a735e63f9d952d1f95a
pramodhsayana <pramodh.sayana@gmail.com>
2024-08-14 22:13:48 +0530

    adding

---
+++ src/ex_10082024/Lab002.py
@@ -0,0 +1,10 @@
+print("helo world ")
+
+print("test", "qa", 123, 3.11, True)
+
+print("test", "qa", 123, 3.11, True, sep='=')
+
+print("test", "qa", 123, 3.11, True, sep='||')
+
+print("test", "qa", 123, 3.11, True, sep='|', end='|')
+print("stk")
